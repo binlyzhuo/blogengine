c59991b7c59905ae24ed1f053cf40de7cf76ad07
binlyzhuo <binlyzhuo@outlook.com>
2015-06-19 10:35:27 +0800

    ~commit

--- BlogEngine/BlogEngine.NET/AppCode/Api/FriendLinkController.cs
+++ BlogEngine/BlogEngine.NET/AppCode/Api/FriendLinkController.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using BlogEngine.Core.Data.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 
 namespace BlogEngine.NET.AppCode.Api
 {
     public class FriendLinkController : ApiController
     {
+        readonly IFriendLinkRepository repository;
+        public FriendLinkController(IFriendLinkRepository repository)
+        {
+            this.repository = repository;
+        }
     }
 }
