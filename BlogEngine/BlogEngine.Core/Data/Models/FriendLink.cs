9f0faf73444ce746d0ac13959919221faada40fa
binlyzhuo <binlyzhuo@outlook.com>
2015-06-19 09:56:42 +0800

    ~commit

--- BlogEngine/BlogEngine.Core/Data/Models/FriendLink.cs
+++ BlogEngine/BlogEngine.Core/Data/Models/FriendLink.cs
@@ -8,10 +8,14 @@ namespace BlogEngine.Core.Data.Models
 {
     public class FriendLink
     {
-        public int ID { set; get; }
+        public int LinkID { set; get; }
         public string Name { set; get; }
         public string Url { set; get; }
         public string Keywords { set; get; }
         public string Contact { set; get; }
+
+        public DateTime AddDate { set; get; }
+
+        public int AddUserID { set; get; }
     }
 }
---
+++ BlogEngine/BlogEngine.NET/AppCode/Api/FriendLinkController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogEngine.NET.AppCode.Api
+{
+    public class FriendLinkController : ApiController
+    {
+    }
+}
